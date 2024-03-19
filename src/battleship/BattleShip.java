638dbe32c7f788ff119221e7c5020645699c5590
s298913 <samuscapellato1@gmail.com>
2024-03-19 21:07:24 +0100

    Added BattleShip class

--- src/battleship/BattleShip.java
+++ src/battleship/BattleShip.java
@@ -1,5 +1,7 @@
-public class App {
+package battleship;
+
+public class BattleShip {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
     }
 }
